aec6623932ae3d1e5d11329e741e1cd138dc5522
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-03-22 22:56:28 +0800

    Update urls.py with some questions (as comments)
     -ideally once admin login, the website should go to ..../personalinfo
    -how to choose 2 columns instead of 1 coz I want composite pri key??? like 'personalinfo/edit<str:id><str:id>' 2 keys

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -18,19 +18,32 @@ from django.urls import path
 
 import app.views
 
+# path('website',views.py method, method name)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', app.views.login, name='login'),
-    path('index', app.views.index, name='index'),
-    path('signup', app.views.signup, name='signup'),
-    path('add', app.views.add, name='add'),
+    path('', app.views.login, name='login'),           ########## penghao: ideally once admin login, the website should go to ..../personalinfo   
+    path('index', app.views.index, name='index'),                        ########     |
+    path('signup', app.views.signup, name='signup'),                      #######    \|/
+    path('personalinfo', app.views.personalinfo, name='personalinfo'),    ####### should go to this line 
+    path('personalcarinfo', app.views.personalcarinfo, name='personalcarinfo'),
+    path('tunavailablecarinfo', app.views.unavailablecarinfo, name='unavailablecarinfo'),
+    path('rentalcarinfo', app.views.rentalcarinfo, name='rentalcarinfo'),
+   #path('add', app.views.add, name='add'),
+    path('addpersonalinfo', app.views.addpersonalinfo, name='addpersonalinfo'),    ######## add info dont need special coz it'll always go same page same empty fields
+    path('addpersonalcarinfo', app.views.addpersonalcarinfo, name='addpersonalcarinfo'),   ######### edit info then need coz diff row diff displayed info in fields
+    path('addunavailablecarinfo', app.views.addunavailablecarinfo, name='addunavailablecarinfo'),
+    path('addrentalcarinfo', app.views.addrentalcarinfo, name='addrentalcarinfo'),
+    
     path('login', app.views.login, name='login'),
-    path('editpersonalinfo', app.views.editpersonalinfo, name='editpersonalinfo'),
-    path('editpersonalcarinfo', app.views.editpersonalcarinfo, name='editpersonalcarinfo'),
-    path('editrentalcarinfo', app.views.editrentalcarinfo, name='editrentalcarinfo'),
+    
+    path('personalinfo/edit/<str:id>', app.views.editpersonalinfo, name='editpersonalinfo'),  ##### how to choose 2 columns instead of 1 coz I want composite pri key
+    path('personalcarinfo/edit/<str:id>', app.views.editpersonalcarinfo, name='editpersonalcarinfo'),  ##### and what is <str:id> ? can <str:email>????
+    path('unavailablecarinfo/edit/<str:id>', app.views.editunavailablecarinfo, name='editunavailablecarinfo'),
+    path('rentalcarinfo/edit/<str:id>', app.views.editrentalcarinfo, name='editrentalcarinfo'),
+    
     path('profile', app.views.profile, name='profile'),
-    path('view/<str:id>', app.views.view, name='view'),
-    path('edit/<str:id>', app.views.edit, name='edit'),
+   #path('view/<str:id>', app.views.view, name='view'),
+   #path('edit/<str:id>', app.views.edit, name='edit'),
 ]
 
