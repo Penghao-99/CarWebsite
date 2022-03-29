d4847eb5fc3352fa89d65c9f543e090b05f006f6
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-03-29 10:41:53 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -37,8 +37,8 @@ urlpatterns = [
     
     path('login', app.views.login, name='login'),
     
-    path('personalinfo/edit/<str:email>', app.views.editpersonalinfo, name='editpersonalinfo'),  ##### how to choose 2 columns instead of 1 coz I want composite pri key
-    path('personalcarinfo/edit/<str:owner>/<str:car_vin>', app.views.editpersonalcarinfo, name='editpersonalcarinfo'),  ##### and what is <str:id> ? can <str:email>????
+    path('personalinfo/edit/<str:email>', app.views.editpersonalinfo, name='editpersonalinfo'),  
+    path('personalcarinfo/edit/<str:owner>/<str:car_vin>', app.views.editpersonalcarinfo, name='editpersonalcarinfo'),  
     path('unavailablecarinfo/edit/<str:car_vin>/<str:unavailable>', app.views.editunavailablecarinfo, name='editunavailablecarinfo'), ### goes to html file editunavailablecarinfo.html 
                                                                                                                ### to retrieve <str:id> from {{cust.0}}
     path('rentalcarinfo/edit/<str:car_vin>/<str:pick_up>', app.views.editrentalcarinfo, name='editrentalcarinfo'),
